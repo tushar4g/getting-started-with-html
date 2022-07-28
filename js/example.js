046993b789ed0288cf391e569de4fea2b927df62
tushar4g <54371737+tushar4g@users.noreply.github.com>
2022-07-28 23:48:58 +0530

    make js file
    run some basics code of javaScript

---
+++ js/example.js
@@ -0,0 +1,42 @@
+// var str;
+// var age=25
+// var str="varVariable";
+// console.log(str, typeof str);
+// console.log(age, (typeof age));
+
+
+// const variable="constVariable";  // declaration is must
+// const number=22;
+// // variable="chnge"     {reassignment is
+// // number=23                not allow  }
+// console.log(variable, typeof variable);
+// console.log(number, (typeof number));
+
+// {let str= "letVariable";
+// let num= 24;
+// num=25;
+// var1="tushar"
+// console.log(str, typeof str);
+// console.log(num, (typeof num));
+// }
+// let a;
+// a=2;
+// console.log(a);
+// console.log(str);
+// console.log(".......................")
+
+// // console.warn("This is warning");
+// // console.error("this is error");
+// // alert("this is alert message")
+
+// **window object :----
+let win=window;
+console.log(win)
+win=window.document;
+console.log(win)
+win=window.innerHeight;
+console.log(win)
+win= window.innerWidth;
+console.log(win)
+win=window.document.location.toString();
+console.log(win)
